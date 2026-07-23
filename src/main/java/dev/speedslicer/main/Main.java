7d5f339359834251449a27da7f29de4b9e92afdc
SpeedSlicer <143555953+SpeedSlicer@users.noreply.github.com>
2026-07-22 22:00:39 -0400

    adada

--- src/main/java/dev/speedslicer/main/Main.java
+++ src/main/java/dev/speedslicer/main/Main.java
@@ -9,20 +9,12 @@ import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
 import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.instance.InstanceManager;
 import net.minestom.server.instance.block.Block;
+import net.minestom.server.network.socket.Server;
 
 public class Main {
+    public static ServerController instance;
     public static void main(String[] args) {
-        MinecraftServer minecraftServer = MinecraftServer.init();
-        InstanceManager instanceManager = MinecraftServer.getInstanceManager();
-        InstanceContainer instanceContainer = instanceManager.createInstanceContainer();
-        instanceContainer.setGenerator(unit -> unit.modifier().fillHeight(0, 40, Block.GRASS_BLOCK));
-        GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
-        globalEventHandler.addListener(AsyncPlayerConfigurationEvent.class, event -> {
-            final Player player = event.getPlayer();
-            event.setSpawningInstance(instanceContainer);
-            player.setRespawnPoint(new Pos(0, 42, 0));
-        });
-        minecraftServer.start("0.0.0.0", 25565);
+        ServerController instance = new ServerController();
     }
 
 }
--- src/main/java/dev/speedslicer/main/ServerController.java
+++ src/main/java/dev/speedslicer/main/ServerController.java
@@ -1,12 +1,37 @@
 package dev.speedslicer.main;
 
 import dev.speedslicer.game.registry.WeaponRegistry;
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.coordinate.Area;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.entity.Player;
+import net.minestom.server.event.GlobalEventHandler;
+import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
+import net.minestom.server.instance.InstanceContainer;
+import net.minestom.server.instance.InstanceManager;
+import net.minestom.server.instance.block.Block;
 
 public class ServerController {
     WeaponRegistry weaponRegistry;
+    InstanceContainer instanceContainer; // TODO replac with actual lobby system
     public ServerController() {
         weaponRegistry = new WeaponRegistry();
+        MinecraftServer minecraftServer = MinecraftServer.init();
 
+        minecraftServer.start("0.0.0.0", 25565);
+    }
+    public void SetupLobby(){
+        InstanceManager instanceManager = MinecraftServer.getInstanceManager();
+        instanceContainer = instanceManager.createInstanceContainer();
+        instanceContainer.setBlockArea(Area.cube(new Pos(0,30,0), 5), Block.STONE);
+    }
+    public void SetupPlayer() {
+        GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
+        globalEventHandler.addListener(AsyncPlayerConfigurationEvent.class, event -> {
+            final Player player = event.getPlayer();
+            event.setSpawningInstance(instanceContainer);
+            player.setRespawnPoint(new Pos(0, 42, 0));
+        });
     }
     public WeaponRegistry getWeaponRegistry() {
         return weaponRegistry;
